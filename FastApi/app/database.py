b80ee6f392ea2d3b4c8cccf89855ac4837e296e4
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-09-17 23:01:17 -0500

    Change data in main

--- FastApi/app/database.py
+++ FastApi/app/database.py
@@ -13,30 +13,28 @@ database = MySQLDatabase(
 
 class ComputerModel(Model):
 
-    id_computer=  AutoField(primary_key=True)
-    manufacturer= CharField(max_length= 50)
-    model= CharField(max_length = 50)
-    processor= CharField(max_length = 50)
-    memory_size= IntegerField()
-    storage_capacity= IntegerField()
-    operating_system= CharField(max_length = 50)
-    graphics_card= CharField(max_length = 50)
-    
-    class meta:
-        database = database
-        tabla_name = "computers"
+    id_computer = AutoField(primary_key=True)
+    manufacturer = CharField(max_length=50)
+    model = CharField(max_length=50)
+    processor = CharField(max_length=50)
+    memory_size = IntegerField()
+    storage_capacity = IntegerField()
+    operating_system = CharField(max_length=50)
+    graphics_card = CharField(max_length=50)
+
+    class Meta:
+        database = database  
+        table_name = "computers"  
 
 class TableModel(Model):
 
-    id_table=  AutoField(primary_key=True)
-    brand= CharField(max_length= 50)
-    model= CharField(max_length = 50)
-    price= DecimalField(max_digits=10, decimal_places=1)
-    support= IntegerField()
-    color= CharField(max_length = 50)
-   
-    
-    class meta:
-        database = database
-        tabla_name = "tables"
+    id_table = AutoField(primary_key=True)
+    brand = CharField(max_length=50)
+    model = CharField(max_length=50)
+    price = DecimalField(max_digits=10, decimal_places=1)
+    support = IntegerField()
+    color = CharField(max_length=50)
 
+    class Meta:
+        database = database  
+        table_name = "tables"  
--- FastApi/app/main.py
+++ FastApi/app/main.py
@@ -1,23 +1,24 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from database import database as connection
+from database import ComputerModel, TableModel
 from routers.computer import computer_route
 from routers.table import table_route
 
 @asynccontextmanager
-async def lifespam(app: FastAPI):
+async def lifespan(app: FastAPI):
 
-    if(connection.is_closed()):
+    if connection.is_closed():
         connection.connect()
+        connection.create_tables([ComputerModel, TableModel])
 
     try:
         yield
-
     finally:
-        if not (connection.is_closed()):
+        if not connection.is_closed():
             connection.close()
 
-app = FastAPI(lifespan = lifespam)
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(computer_route, prefix="/api/computers", tags=["computers"])
 app.include_router(table_route, prefix="/api/tables", tags=["tables"])
