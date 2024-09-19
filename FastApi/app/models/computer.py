eeadaeed0ab608f5cbc272b8bbb4d439ece6bbc9
julianaMarin12 <142064728+julianaMarin12@users.noreply.github.com>
2024-09-19 07:41:17 -0500

    class documentation

--- FastApi/app/models/computer.py
+++ FastApi/app/models/computer.py
@@ -1,22 +1,54 @@
-from pydantic import BaseModel, validator, Field 
+"""Computer model model.
+BaseModel:The base class for creating data models using pydantic.
+validator: A decorator provided by pydantic to define custom validation methods for fields.
+Field: A function used to specify constraints, metadata, and validation rules on fields. 
+"""
+
+from pydantic import BaseModel, validator, Field
 
 class Computer(BaseModel):
+    """Computer model 
+    This model defines the structure and attributes of a computer entity.
+
+    Attributes:
+        id (int): Represents the unique identifier for the computer.
+        manufacturer(str): The manufacturer of the computer. 
+        model(str):The model name of the computer. This is a string field.
+        processor(str):Describes the processor in the computer.
+        memory_size (int): Represents the size of the computers memory in gigabytes. 
+        storage_capacity: int:  This field represents the storage capacity of the computer.
+        operating_system(str):The operating system installed on the computer.
+        graphics_card(str): The graphics card used in the computer.
+    """
     id: int
     manufacturer: str
     model: str
     processor: str
-    memory_size: int 
+    memory_size: int
     storage_capacity: int = Field(..., gt=0, le= 1000)
     operating_system: str
     graphics_card: str
-    
+
+    """Memory size validation
+    Validator:
+        @validator('memory_size'):This is a custom validation method for the memory_size field.
+        Validation logic: The method ensures the memory_size value.
+    """
 
     @validator('memory_size')
-    def display_size_must_be_positive_and_max_four(cls, v):
+    def display_size_must_be_positive_and_max_four(cls,v):
+        """
+        Verify that the memory size is positive and not greater than 129.
+        Args:
+            v (int): The value of the memory size to validate.
+        Returns:
+            int: The value validated if correct.
+        Raises:
+            ValueError: If the memory size is less than or equal to 0 or greater than 129.
+        """
         if v <= 0 or v > 129:
             raise ValueError('display_size must be a positive float and cannot exceed 129')
-        return v
-
+        return v       
+    
     class Config:
         anystr_strip_whitespace = True
-
--- FastApi/app/models/table.py
+++ FastApi/app/models/table.py
@@ -1,11 +1,28 @@
-from pydantic import BaseModel, validator, Field
+"""Table model model.
+BaseModel:The base class for creating data models using pydantic.
+validator: A decorator provided by pydantic to define custom validation methods for fields.
+Field: A function used to specify constraints, metadata, and validation rules on fields. 
+"""
+
+from pydantic import BaseModel, validator
 
 class Table(BaseModel):
+    """Table model
+    This model defines the structure and attributes of a table entity.
+
+    Attributes:
+        id (int): Represents the unique identifier for the computer. 
+        brand (str): The brand name for the table.
+        model (str): The model name for the table.
+        price (float): The price of the table.
+        support (int): The number of support points for the table.
+        color (str): The color of the table.
+    """
     id: int
     brand: str
     model: str
     price: float
-    support: int 
+    support: int
     color: str
     
     @validator('support')
